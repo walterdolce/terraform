5e803c94a63680c3655645b64f375a624843b838
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-10-20 10:46:32 -0700

    terraform: disable shadowing destroy graph for now
    It doesn't fully work so we want to wait until we think its ready before
    we start the shadowing.

--- terraform/context.go
+++ terraform/context.go
@@ -395,7 +395,7 @@ func (c *Context) Apply() (*State, error) {
 			Module:    c.module,
 			Diff:      c.diff,
 			State:     c.state,
-			Providers: c.providersList(),
+			Providers: c.components.ResourceProviders(),
 		}).Build(RootModulePath)
 	} else {
 		newGraph, err = (&ApplyGraphBuilder{
@@ -435,6 +435,11 @@ func (c *Context) Apply() (*State, error) {
 		log.Printf("[WARN] terraform: real graph is experiment, shadow is experiment")
 		real = shadow
 	} else {
+		// TODO: remove before branch is done, we're just not ready for this yet
+		if c.destroy {
+			shadow = nil
+		}
+
 		log.Printf("[WARN] terraform: real graph is original, shadow is experiment")
 	}
 
